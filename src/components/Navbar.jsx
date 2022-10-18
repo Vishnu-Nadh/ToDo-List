375c5415ea23f1256ecfae167bec55f39d99d75b
Vishnu-Nadh <vishnunadh222@gmail.com>
2022-10-18 12:53:27 +0530

    navbar design and functionality completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,32 @@ import styles from "./Navbar.module.css";
 import ThemeContext from "../context/theme-context";
 
 const Navbar = () => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { themeColor, toggleTheme } = useContext(ThemeContext);
   return (
     <div className={styles.nav}>
       <div className={styles.nav__contents}>
         <h2 className={styles.nav__logo}>ToDo</h2>
-        <button
-          type="button"
+        <label
+          // type="button"
           className={styles.nav__theme_toggle}
-          onClick={toggleTheme}
+          // onClick={toggleTheme}
+          htmlFor="check-box"
         >
-          themeButton
-        </button>
+          <div>
+            <input
+              type="checkbox"
+              className={styles["check-box"]}
+              id="check-box"
+              checked={themeColor === "light" ? true : false}
+              name="check-box"
+              onChange={toggleTheme}
+            ></input>
+            <div className={`${styles.nav__theme_icon}`}>
+              {themeColor === "dark" && <i class="fa-solid fa-moon"></i>}
+              {themeColor === "light" && <i class="fa-solid fa-sun"></i>}
+            </div>
+          </div>
+        </label>
       </div>
     </div>
   );
